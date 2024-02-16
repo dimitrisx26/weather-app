586ad393c622906a1edf2f8c5fed1196270dced9
dimitrisx26 <dxynos@outlook.com>
2024-02-17 00:51:41 +0200

    displaying, searching and caching current weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { WeatherService } from './weather.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  defaultCity: string = 'Athens';
   searchInput: string = '';
+  weatherData: any = {};
+  errorMessage: string = '';
+  showError: boolean = false;
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit() {
+    this.weatherService.getWeather(this.defaultCity).subscribe((data: any) => {
+      this.weatherData = data;
+    });
+  }
 
   onSearchLocation(city: string) {
-    console.log(city);
+    if (city === '') {
+      return;
+    }
+    this.weatherService.getWeather(city).subscribe({
+      next: (data: any) => {
+        this.weatherData = data;
+        this.showError = false;
+      },
+      error: (error) => {
+        console.log('Error: ', error);
+        this.errorMessage = 'Error finding city, please try again.';
+        this.showError = true;
+        setTimeout(() => {
+          this.showError = false;
+        }, 3000);
+      },
+    });
   }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of, switchMap, timer } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  api = '&appid=75cec7c3f4300600d64abf22fc7a2269&units=metric';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string) {
+    return timer(0, 15 * 60 * 1000).pipe(
+      switchMap(() => {
+        const cachedDataString = localStorage.getItem(city);
+        const cachedData = cachedDataString
+          ? JSON.parse(cachedDataString)
+          : null;
+        const currentTime = Date.now();
+        if (cachedData && currentTime - cachedData.timestamp < 15 * 60 * 1000) {
+          return of(cachedData.data);
+        } else {
+          const request = this.http.get(this.weatherURL + city + this.api);
+          request.subscribe((data) => {
+            localStorage.setItem(
+              city,
+              JSON.stringify({ data, timestamp: currentTime })
+            );
+          });
+          return request;
+        }
+      })
+    );
+  }
 }
