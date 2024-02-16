0415553ea51617396b595ce5db352ad543120c27
dimitrisx26 <dxynos@outlook.com>
2024-02-16 11:42:50 +0200

    Add search functionality to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  searchInput: string = '';
 
+  onSearchLocation(city: string) {
+    console.log(city);
+  }
 }
